b17e2a0f05e7c2984339ebda7b2e290c706a875c
JonathanNi <njj@JonathanNideMacBook-Pro.local>
2021-04-13 00:07:01 +0800

    new interest setting

--- Algorithms/EQH_VI.py
+++ Algorithms/EQH_VI.py
@@ -4,6 +4,10 @@ from Algorithms.EQH import EQH
 class EQH_VI(EQH):
 
     def get_interest(self, s, a):
+        if s in [(2, 0)] and a == self.task.ACTION_UP:
+            return 0.1
+        else:
+            return 1
         if (s[0] == 0 and a == self.task.ACTION_UP) or (s[0] == 5 and a == self.task.ACTION_DOWN) \
             or (s[1] == 0 and a == self.task.ACTION_LEFT) or (s[1] == 8 and a == self.task.ACTION_RIGHT):
             return 0.8
--- Plotting/plot_params.py
+++ Plotting/plot_params.py
@@ -12,6 +12,7 @@ DEBUG_MODE = True
 # noinspection SpellCheckingInspection
 COLORS = ['#000000', "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22",
           "#17becf", '#000000', "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22",
+          "#17becf", '#000000', "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22",
           "#17becf"]
 ALG_COLORS = {alg_name: color for alg_name, color in zip(alg_dict.keys(), COLORS)}
 ALG_COLORS['LSTD'] = ALG_COLORS['TD']
@@ -29,14 +30,18 @@ AUC_AND_FINAL = ['auc', 'final']
 EXP_ATTRS = {'FirstChain': FirstChainAttr, 'FirstFourRoom': FirstFourRoomAttr, '1HVFourRoom': HVFirstFourRoomAttr,'NewChain': FirstChainAttr,'NewFourRoom': FirstFourRoomAttr,'NewHVFourRoom': HVFirstFourRoomAttr,'MountainCar': MountainCarAttr,'DynaMaze': DynaMazeAttr}
 
 if DEBUG_MODE:
-    EXPS = ['MountainCar']
+    EXPS = ['DynaMaze']
     #ALGS = ['SARSA', 'ESARSA', 'ESARSAH']
+    #ALGS = ['SARSA', 'ESARSA_VI', 'ESARSAH_VI']
     #ALGS = ['Q', 'EQ', 'EQH']
+    ALGS = ['Q', 'EQ_VI', 'EQH_VI']
     #ALGS = ['SARSA', 'ESARSA', 'ESARSAH', 'Q', 'EQ', 'EQH']
-    ALGS = ['SARSA',  'ESARSA', 'Q',  'EQ']
+    #ALGS = ['SARSA',  'ESARSA', 'Q',  'EQ']
     LMBDA_AND_ZETA = [0.0]
     AUC_AND_FINAL = ['auc']
     #ALG_GROUPS = {'new': ['SARSA', 'ESARSA', 'ESARSAH']}
+    #ALG_GROUPS = {'new': ['SARSA', 'ESARSA_VI', 'ESARSAH_VI']}
     #ALG_GROUPS = {'new': ['Q', 'EQ', 'EQH']}
+    ALG_GROUPS = {'new': ['Q', 'EQ_VI', 'EQH_VI']}
     #ALG_GROUPS = {'new': ['SARSA', 'ESARSA', 'ESARSAH', 'Q', 'EQ', 'EQH']}
-    ALG_GROUPS = {'new': ['SARSA', 'ESARSA', 'Q', 'EQ']}
+    #ALG_GROUPS = {'new': ['SARSA', 'ESARSA', 'Q', 'EQ']}
